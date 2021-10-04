e6d904b8691b29c26bbd8b9b3c07af184c21b11e
ITigor1985 <xromer85@gmail.com>
2021-10-04 14:38:34 +0300

    task-02 complit

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulEl = document.querySelector("#ingredients");
+
+const newEl = ingredients.map(ingredient => {
+  const liEl = document.createElement("li");
+  liEl.classList.add("item");
+  liEl.textContent=ingredient;
+  return liEl;
+})
+ulEl.append(...newEl)
