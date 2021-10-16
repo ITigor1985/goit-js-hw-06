1a7800976ade54b23d7b423c99b749ae9fe2a19d
Igor Tarasov <xromer85@gmail.com>
2021-10-16 16:45:13 +0300

    task9 compl

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
+const registrationData = {};
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("все поля должны быть заполнены");
+  }
+  registrationData.email = email.value;
+  registrationData.password = password.value;
+
+  console.log(registrationData);
+  event.currentTarget.reset();
+}
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector(".change-color");
+const spanColorEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+const changeColor = () => {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  spanColorEl.textContent = color;
+};
+
+btn.addEventListener("click", changeColor);
